470a9cca9d3088089551501dfdd79a0e749effa7
ismawanto <100463647+ismawanto@users.noreply.github.com>
2022-10-07 08:08:46 +0700

    Update ExampleInstrumentedTest.kt

--- app/src/androidTest/java/room/demo/roomdemo/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/room/demo/roomdemo/ExampleInstrumentedTest.kt
@@ -9,15 +9,15 @@ import org.junit.runner.RunWith
 import org.junit.Assert.*
 
 /**
- * Instrumented test, which will execute on an Android device.
+ * Pada bagian ini terjadi pengujian instrumen, yang akan dijalankan.
  *
- * See [testing documentation](http://d.android.com/tools/testing).
+ * 
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
-        // Context of the app under test.
+        // Pada bagian ini adalah konteks aplikasi yang akan diuji
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.demo.roomdemo", appContext.packageName)
     }
